72c92e64eda91af3e6c5996d22bfb20d8d92a520
Maxi <machugab@gmail.com>
2025-01-06 19:09:19 -0300

    :(

---
+++ CodigoBasicoC/0_basico.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("\nHello Word mi nombre es \"Maxi\" \n");
+
+    printf("1\t 2\t 3\t\n");
+
+    return 0; // retorna 0 porque el programa termino sin problemas
+}
