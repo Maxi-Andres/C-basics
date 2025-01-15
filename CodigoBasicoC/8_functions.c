276e618f2386bfecacde05a3920d78d19bcf9890
Maxi <machugab@gmail.com>
2025-01-15 17:15:08 -0300

    more

---
+++ CodigoBasicoC/8_functions.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void ejemplo(){
+
+    printf("hola");
+
+}
+
+int main(){
+
+    ejemplo();
+    ejemplo();
+
+    return 0;
+}
+
+
+//! 1:32:00 falta random num y los prints que no estan en el tuto
